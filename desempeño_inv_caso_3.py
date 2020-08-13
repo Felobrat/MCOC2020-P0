f1bcc0d402846d5ff51ed551fedec037fd529393
Felobrat <69157203+Felobrat@users.noreply.github.com>
2020-08-12 21:04:21 -0400

    Add files via upload

---
+++ desempeño_inv_caso_3.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  7 18:11:22 2020
+
+@author: Felipe Bravo
+"""
+#FUNCIONES A USAR
+from psutil import virtual_memory
+import scipy as sp
+import matplotlib.pyplot as plt
+import numpy as np
+
+#ROTULO DE LOS EJES
+x     = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000]
+y1    = [0.000001, 0.1e-3, 1e-3, 10e-3, 0.1, 1, 10, 60, 600]
+y2    = [10**i for i in range(3, 11)]
+y1val = [".","0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min", "10 min"]
+y2val = ["1 KB","10 KB","100 KB","1 MB","10 MB", "100 MB","1 GB", "10 GB"]
+
+
+
+#EMPEZAR A GRAFICAR
+fig = plt.figure()  
+
+#CREAR PRIMER GRAFICO Y ORDENARLO EN EL PRIMER ESPACIO VERTICALMENTE
+graf1 = fig.add_subplot(2,1,1)
+
+#RECORRER LOS .txt CREADOS Y ALMACENAR LA INFORMACION PARA USARLA
+datos = np.loadtxt('timing_inv_caso_3_np.half.txt')
+graf1.plot(datos[:,0], datos[:,1],"o-", label='half')
+datos = np.loadtxt('timing_inv_caso_3_np.single.txt')
+graf1.plot(datos[:,0], datos[:,1],"o-", label='single')
+datos = np.loadtxt('timing_inv_caso_3_np.double.txt')
+graf1.plot(datos[:,0], datos[:,1],"o-", label='double')
+datos = np.loadtxt('timing_inv_caso_3_np.longdouble.txt')
+graf1.plot(datos[:,0], datos[:,1],"o-", label='longdouble')
+
+#AJUSTAR A ESCALA LOG-LOG EL GRAFICO 1
+graf1.set_xscale('log')
+graf1.set_yscale("log")
+
+#DARLE EL ESPACIADO ASIGNADO
+graf1.set_xticks(x)
+#HACER QUE NO TENGA VALORES
+graf1.set_xticklabels(['' for _ in range(len(x))])
+
+#DARLE ESPACIADO Y VALORES AL EJE 'Y' SEGUN EL PRINCIPIO
+graf1.set_yticks(y1)
+#AJUSTAR LOS VALORES DEL EJE PARA MOSTRAR EN KB, MB, GB
+graf1.set_yticklabels(y1val)
+#ACTIVAR LA GRILLA DEL GRAFICO
+graf1.grid()
+#HACER QUE TENGA LOS ESPACIOS ENTREMEDIO, SEÑALANDO AL ESCALA LOG
+graf1.minorticks_on()
+graf1.legend()
+
+#DARLE TITULO A LOS EJES
+graf1.set_ylabel('Tiempo Transcurrido (s)')
+graf1.set_title('Rendimiento INV (CASO 3)')
+
+
+#CREAR SEGUNDO GRAFICO Y ORDENARLO EN EL SEGUNDO ESPACIO VERTICALMENTE
+graf2 = fig.add_subplot(2,1,2)
+
+#REUSAR LOS DATOS ALMACENADOS PREVAIMENTE EN 'datos'
+datos = np.loadtxt('timing_inv_caso_3_np.half.txt')
+graf2.plot(datos[:,0], datos[:,2],"o-", label='half')
+datos = np.loadtxt('timing_inv_caso_3_np.single.txt')
+graf2.plot(datos[:,0], datos[:,2],"o-", label='single')
+datos = np.loadtxt('timing_inv_caso_3_np.double.txt')
+graf2.plot(datos[:,0], datos[:,2],"o-", label='double')
+datos = np.loadtxt('timing_inv_caso_3_np.longdouble.txt')
+graf2.plot(datos[:,0], datos[:,2],"o-", label='longdouble')
+
+#AJUSTAR A ESCALA LOG-LOG EL GRAFICO 2
+graf2.set_yscale("log")
+graf2.set_xscale("log")
+
+#DARLE EL ESPACIADO ASIGNADO
+graf2.set_xticks(x)
+#HACER QUE TENGA LOS VALORES DE ITERACION 
+graf2.set_xticklabels(x)
+#ROTAR LAS ETIQUETAS COMO EL DE EJEMPLO
+for tick in graf2.get_xticklabels():
+    tick.set_rotation(60)
+
+#DARLE ESPACIADO AL EJE 
+graf2.set_yticks(y2)
+#REASIGNAR LOS VALORES DE LOS EJES 
+graf2.set_yticklabels(y2val)
+#DEFINE UNA LINEA HORIZONTAL ACCEDIENDO A LA MEMORIA VIRTUAL DEL COMPUTADOR Y GRAFICANDOLA
+graf2.axhline(virtual_memory().total, linestyle='--', color='black')
+
+
+#ACTIAR LA GRILLA
+graf2.grid()
+#HACER QUE TENGA LOS ESPACIOS ENTREMEDIO, SEÑALANDO AL ESCALA LOG
+graf2.minorticks_on()
+graf2.legend()
+#LIMITE DE LOS EJES
+
+
+graf2.set_ylim([10, 10**11])
+#TITULO DE LOS EJES
+graf2.set_xlabel("Tamaño Matriz N")
+graf2.set_ylabel("Uso memoria")
+
+
+#GUARDAR COMO IMAGEN EL GRAFICO
+fig.savefig('RENDIMIENTO_CASO3.png')
---
+++ timing_inv_caso_2_single.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 15:20:02 2020
+
+@author: Felipe Bravo
+"""
+
+
+from time import perf_counter 
+from scipy.linalg import inv
+import numpy as np
+
+NP = [2, 5, 10,
+    12, 15, 20,
+    30, 40, 45, 
+    50, 55, 60, 
+    75, 100, 125, 
+    160, 200, 250, 
+    350, 500, 600, 
+    800, 1000, 2000,
+    5000, 10000] #indice de dimension de matrices cuadradas que vamos a evaluar
+
+dts = [] #indice que almacena el tiempo que se demora en cada iteracion de matriz de dimension N en NP
+mem = [] #indice que almacena la memoria que se usa en cada iteracion de matriz de dimension N en NP
+name = (f"timing_inv_caso_2_np.single.txt")
+fid = open(name, "w") #genera el archivo Matmul.txt
+for N in NP: #recorremos el indice NP para matrices aleatorias A y B de dimension N en NP
+    print(f"N = {N}") #imprime en consola lo que está pasando en la operacion, indicando en que elemento N en NP esta iterando.
+    A = 2*(np.eye(N, dtype=np.single)) #genera la matriz A diagonal con valores 2 de dimension NxN
+    for m in range(N):
+        for n in range(N):
+            if m==n+1:
+                A[m,n]+=-1
+                if m==n-1:
+                    A[m,n]+=-1
+    print(A.dtype)    
+    t1 = perf_counter() #empieza a contar cuando llega a esta parte de la iteracion
+    Ainv = inv(A, overwrite_a=False) #hace la operacion de multiplicar matrices A y B de dimension NxN*NxN
+    t2 = perf_counter() #deja de contar
+        
+    dt = t2 - t1 #mide cuando se demoro en multiplicar las matrices.
+    print(Ainv.dtype)
+    size=(Ainv.itemsize+A.itemsize)*(N**2) #calculo de cuanta memoria usa en alamcenar los datos de las matrices.
+    print(Ainv.itemsize)
+    dts.append(dt) #guarda en el indice dts creado el timepo de iteracion que se demoró para esa matriz e NxN
+    mem.append(size) #guarda en el indice mem creado lo que uso de memoria en la iteracion para esa matriz de NxN
+
+    fid.write(f"{N} {dt} {size}\n") #escribe en el archivo los datos almacenados
+        
+    print(f"Tiempo transcurrido = {dt} s") #imprime en la consola para llevar un registro visual de lo que esta haciendo
+    print(f"memoria usada = {size} bytes")
+    fid.flush() #limpia lo almacenado que imprimio en el archivo de texto
+    
+fid.close() #deja de escribir
